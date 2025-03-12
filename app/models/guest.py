b14ba832410933bb794cf733c210846b1a8827eb
PedroPereiraGuimaraes <pedropeguimaraes@gmail.com>
2025-03-12 11:16:54 -0300

    fix: fixed pydantic arguments

--- app/models/guest.py
+++ app/models/guest.py
@@ -18,8 +18,8 @@ class Guest(BaseModel):
     dependents: List[Dependent] = Field(...)
     
     class Config:
-        min_anystr_length = 1
-        anystr_strip_whitespace = True
+        str_min_length = 1
+        str_strip_whitespace = True
 
     def __repr__(self):
         return f"Guest({self.accountable}, {self.email}, {len(self.dependents)} dependents)"
